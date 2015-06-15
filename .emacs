62c3f749932989b532979cddb9011217128b1b04
zhouqz815 <zhouqianzhong@gmail.com>
2015-06-15 19:12:06 +0800

    sdfsdf

--- .emacs
+++ .emacs
@@ -6,7 +6,7 @@
 ;;这里是设置默认的el文件的地方，也可以改成其它地方
 (add-to-list 'load-path "~/.emacs.d/")
 (add-to-list 'load-path "~/.emacs.d/init")
-
+(add-to-list 'load-path "~/.emacs.d/addon")
 (load "init-base")
 (load "init-key")
 (load "init-addon")
--- .emacs.d/init/init-addon.el
+++ .emacs.d/init/init-addon.el
@@ -14,3 +14,10 @@
 (tabbar-mode 1)
 (global-set-key [(meta j)] 'tabbar-backward)
 (global-set-key [(meta k)] 'tabbar-forward)
+
+
+(autoload 'markdown-mode "markdown-mode"
+    "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
