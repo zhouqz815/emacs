12b8b2c5cead489c42e1cc26d702c0507043125c
zhouqz815 <zhouqianzhong@gmail.com>
2015-06-15 15:42:46 +0800

    sdfs

--- .emacs.d/init/init-addon.el
+++ .emacs.d/init/init-addon.el
@@ -6,16 +6,8 @@
 ;;(color-theme-matrix)
 (color-theme-dark-blue2)
 ;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-  (load
-    (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                      ;;   ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 ;;--------------------插件tabbar设置---------------------
 (require 'tabbar)
